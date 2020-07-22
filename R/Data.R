b8c4ff355f384ca341713824a16ceb22f6ed179d
cheungngo <cheungngo@gmail.com>
2020-07-22 21:21:23 +0800

    20200722a

---
+++ R/Data.R
@@ -0,0 +1,20 @@
+#' masterdata_A
+#'
+#' A dataset containing all the independent variables (non psychiatric related) in a survey with regard to premature ejaculation
+#'
+#' @format A dataframe with 229 subjects (rows) and 103 variables (columns)
+"masterdata_A"
+
+#' masterdata_B
+#'
+#' A dataset containing all the independent variables (psychiatric related) in a survey with regard to premature ejaculation
+#'
+#' @format A dataframe with 229 subjects (rows) and 39 variables (columns)
+"masterdata_B"
+
+#' dependent_var
+#'
+#' A dataset containing all the dependent variables in a survey with regard to premature ejaculation
+#'
+#' @format A dataframe with 229 subjects (rows) and 18 variables (columns)
+"dependent_var"
